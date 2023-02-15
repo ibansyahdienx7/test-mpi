90243c0d294b1a6c75c0cea5cf6f8cb7b7719c64
Ibansyah <ibansyahdienx7@gmail.com>
2023-02-15 20:40:45 +0700

    update

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Traits\MyHelper;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    use MyHelper;
+
+    function list()
+    {
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'photo_profile',
+        'role',
+        'is_login',
+        'login_at',
+        'pzn',
     ];
 
     /**
